91ee522652cdc281562bd1a6285fe70b4fc76e06
Jo4096 <121760445+Jo4096@users.noreply.github.com>
2025-08-06 10:34:54 +0100

    Update MQTT_Agent.hpp

--- src/MQTT_Agent.hpp
+++ src/MQTT_Agent.hpp
@@ -5,6 +5,7 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <functional>
+#include <vector>
 
 // Constantes
 #ifndef MAX_KNOWN_DEVICES
@@ -20,19 +21,21 @@
 #endif
 
 // Estrutura para os manipuladores de comandos
-struct CommandHandler {
+struct CommandHandler
+{
     String name;
-    std::function<void(String, String, JsonDocument&)> handler;
+    std::function<void(String, String, JsonDocument &)> handler;
 };
 
-class MQTT_Agent {
+class MQTT_Agent
+{
 private:
-    const char* ssid;
-    const char* password;
-    const char* mqttServer;
-    const char* mqttUsername;
-    const char* mqttPassword;
-    const char* deviceId;
+    const char *ssid;
+    const char *password;
+    const char *mqttServer;
+    const char *mqttUsername;
+    const char *mqttPassword;
+    const char *deviceId;
     int mqttPort;
     long lastPingTime = 0;
     int pingPeriod;
@@ -40,39 +43,34 @@ private:
     WiFiClient wifiClient;
     PubSubClient mqttClient;
 
-    const char* subscribedTopics[MAX_TOPICS];
-    int subscribedCount = 0;
-
-    CommandHandler commandHandlers[MAX_COMMANDS];
-    int commandCount = 0;
-
-    String knownDevices[MAX_KNOWN_DEVICES];
-    int knownCount = 0;
+    std::vector<String> subscribedTopics;
+    std::vector<CommandHandler> commandHandlers;
+    std::vector<String> knownDevices;
 
     std::function<void(String, String, String)> onMessageCallback;
 
-    static void internalCallback(char* topic, byte* payload, unsigned int length);
+    static void internalCallback(char *topic, byte *payload, unsigned int length);
     void handleMessage(String topic, String payload);
-    void addKnownDevice(const String& deviceId);
+    void addKnownDevice(const String &deviceId);
 
     // Métodos internos para ping e pong
-    void _handle_ping_command(String from, String topic, JsonDocument& doc);
+    void _handle_ping_command(String from, String topic, JsonDocument &doc);
     void _send_ping();
 
 public:
-    MQTT_Agent(const char* ssid, const char* password, const char* mqttServer, const char* mqttUsername, const char* mqttPassword, const char* deviceId, int port = 1883, int pingPeriod = 30000);
+    MQTT_Agent(const char *ssid, const char *password, const char *mqttServer, const char *mqttUsername, const char *mqttPassword, const char *deviceId, int port = 1883, int pingPeriod = 30000);
 
-    void begin(bool enablePing=true);
+    void begin(bool enablePing = true);
     void loop();
-    void addSubscriptionTopic(const char* topic);
-    void publish(const String& topic, const String& message);
-    void publishToDevice(const String& devId, const String& message);
-    void publishToDeviceFormatted(const String& devId, const String& command, const String& message);
+    void addSubscriptionTopic(String topic);
+    void publish(const String &topic, const String &message);
+    void publishToDevice(const String &devId, const String &message);
+    void publishToDeviceFormatted(const String &devId, const String &command, const String &message);
     void setOnMessageCallback(std::function<void(String, String, String)> callback);
-    void registerCommand(const String& name, std::function<void(String, String, JsonDocument&)> handler);
-    const char* getDeviceId() { return deviceId; }
+    void registerCommand(const String &name, std::function<void(String, String, JsonDocument &)> handler);
+    const char *getDeviceId() { return deviceId; }
 };
 
-void default_PongResponse(String from, String topic, JsonDocument& doc);
+void default_PongResponse(String from, String topic, JsonDocument &doc);
 
 #endif
